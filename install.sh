09ec228d207d2bd118c2b484ac240ebe9ca42736
Simone <simone.denada@gmail.com>
2019-12-29 14:45:45 +0100

    fixed dotfiles path

--- install.sh
+++ install.sh
@@ -2,18 +2,24 @@
 # SymLink config files
 # ln -sfn should update symlinks if an existing duplicate file is found
 
-# Dotfiles directory (for Symlinks)
-DOTFILES_PATH=$(dirname $0)
+
+# Get Dotfiles directory (for Symlinks)
+CURRENT_PATH=$(pwd) # Store current location
+REL_DOTFILES_PATH=$(dirname $0) # Relative dotfiles path
+ABS_DOTFILES_PATH=$(cd $REL_DOTFILES_PATH && pwd) # Absolute dotfiles path
+cd $CURRENT_PATH # return to previous location
+
 
 # ZSH and Prezto
-ln -sfn $DOTFILES_PATH/zsh/.zlogin ~/.zlogin
-ln -sfn $DOTFILES_PATH/zsh/.zlogout ~/.zlogout
-ln -sfn $DOTFILES_PATH/zsh/.zpreztorc ~/.zpreztorc
-ln -sfn $DOTFILES_PATH/zsh/.zprofile ~/.zprofile
-ln -sfn $DOTFILES_PATH/zsh/.zshenv ~/.zshenv
-ln -sfn $DOTFILES_PATH/zsh/.zshrc ~/.zshrc
+ln -sfn $ABS_DOTFILES_PATH/zsh/.zlogin ~/.zlogin
+ln -sfn $ABS_DOTFILES_PATH/zsh/.zlogout ~/.zlogout
+ln -sfn $ABS_DOTFILES_PATH/zsh/.zpreztorc ~/.zpreztorc
+ln -sfn $ABS_DOTFILES_PATH/zsh/.zprofile ~/.zprofile
+ln -sfn $ABS_DOTFILES_PATH/zsh/.zshenv ~/.zshenv
+ln -sfn $ABS_DOTFILES_PATH/zsh/.zshrc ~/.zshrc
+
 
 # Git
-if [ -f $DOTFILES_PATH/private/.gitconfig ]; then
-    ln -sfn $DOTFILES_PATH/private/.gitconfig ~/.gitconfig
+if [ -f $ABS_DOTFILES_PATH/private/.gitconfig ]; then
+    ln -sfn $ABS_DOTFILES_PATH/private/.gitconfig ~/.gitconfig
 fi
