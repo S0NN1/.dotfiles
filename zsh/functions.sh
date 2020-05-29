11e6b8bb245d5e9efda0529f0d1ff734c8af4fbc
Simone <simone.denada@gmail.com>
2020-05-29 17:59:08 +0200

    Added git-large-files function to find big files inside git repos

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -183,3 +183,14 @@ function m3ugen() {
   unset ORDER
   unset PLAYLIST_NAME
 }
+
+
+# Function useful to find "big" files inside a git repo
+function git-large-files() {
+	git rev-list --objects --all \
+	| git cat-file --batch-check='%(objecttype) %(objectname) %(objectsize) %(rest)' \
+	| sed -n 's/^blob //p' \
+	| sort --numeric-sort --key=2 \
+	| cut -c 1-12,41- \
+	| $(command -v gnumfmt || echo numfmt) --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
+}
