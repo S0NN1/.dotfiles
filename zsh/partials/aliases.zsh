05f26db5cace63b267669383ae598c46957fc0df
Simone <simone.denada@gmail.com>
2020-12-27 16:11:24 +0100

    macOS aliases fix

--- zsh/partials/aliases.zsh
+++ zsh/partials/aliases.zsh
@@ -4,9 +4,9 @@
 alias .u="update-dotfiles"
 
 # LS aliases
-alias l='ls -1ah --color=auto'
-alias la='ls -lah --color=auto'
-alias ls='ls -G --color=auto'
+alias l='ls -1ah'
+alias la='ls -lah'
+alias ls='ls -G'
 
 # Git
 alias gl="git pull"
@@ -14,5 +14,25 @@ alias glog='git log --topo-order --pretty=format:"${_git_log_medium_format}"'
 alias gitgud="git add . ; git commit -m'(ノಠ益ಠ)ノ彡┻━┻' ; git push"
 
 
+# Aliases based on OS Type
+case "$OSTYPE" in
 
+  # ~ macOS ~
+  darwin*)
+   
+  ;;
+  
 
+  # ~ WSL (Linux on Windows 10) ~
+  linux-gnu)
+    alias l='ls -1ah --color=auto'
+    alias la='ls -lah --color=auto'
+    alias ls='ls -G --color=auto'
+  ;;
+  
+  
+  # ~ FreeBSD (TrueNAS) ~
+  freebsd*)
+   
+  ;;
+esac
